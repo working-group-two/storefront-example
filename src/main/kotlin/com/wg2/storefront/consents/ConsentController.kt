d73921ae4bb021546cc8dc3419500a220ea04acc
Håkon Bø <hakon@wgtwo.com>
2023-06-16 08:45:44 +0100

    already checked

--- src/main/kotlin/com/wg2/storefront/consents/ConsentController.kt
+++ src/main/kotlin/com/wg2/storefront/consents/ConsentController.kt
@@ -6,21 +6,18 @@ import io.javalin.http.Context
 object ConsentController {
 
     fun listConsentsForSubscriber(ctx: Context) {
-        assert(ctx.currentUser != null) { "Not logged in" }
         val phoneNumber = ctx.currentUser!!
         val consents = ConsentService.getConsentsForSubscriber(phoneNumber)
         ctx.json(consents)
     }
 
     fun revokeConsentForSubscriber(ctx: Context) {
-        assert(ctx.currentUser != null) { "Not logged in" }
         val phoneNumber = ctx.currentUser!!
         val productId = ctx.pathParam("productId")
         ConsentService.revokeConsentForSubscriber(phoneNumber, productId)
     }
 
     fun createConsentForSubscriber(ctx: Context) {
-        assert(ctx.currentUser != null) { "Not logged in" }
         val phoneNumber = ctx.currentUser!!
         val productId = ctx.pathParam("productId")
         val productVersion = ctx.pathParam("productVersion")
